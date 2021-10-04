549f474903a36e9946aaa6679102e31a38cb0e93
Salvador Navas <60132832+navass11@users.noreply.github.com>
2021-10-04 19:39:15 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
-long_description = (this_directory / "README.md").read_text()
+#long_description = (this_directory / "README.md").read_text()
+with open(this_directory+"README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
 
 setup(  name='NEOPRENE',
         packages = find_packages(),
