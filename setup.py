fedce6e7f1101086e58bf8b97b5ee3080d004024
Manuel del Jesus <manuel.deljesuspe@gmail.com>
2021-10-04 19:14:32 +0200

    Code reorganized to allow to pip install install it
    I have moved the directories of the sublibraries and modified the setup
    file to allow users to install the library using pip.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	renamed:    NSRP/Calibration.py -> NEOPRENE/NSRP/Calibration.py
    	renamed:    NSRP/HiperParams.py -> NEOPRENE/NSRP/HiperParams.py
    	renamed:    NSRP/MathematicalPropertiesNSRP.py -> NEOPRENE/NSRP/MathematicalPropertiesNSRP.py
    	renamed:    NSRP/PSOMdJ.py -> NEOPRENE/NSRP/PSOMdJ.py
    	renamed:    NSRP/README.md -> NEOPRENE/NSRP/README.md
    	renamed:    NSRP/Simulation.py -> NEOPRENE/NSRP/Simulation.py
    	renamed:    NSRP/Statistics.py -> NEOPRENE/NSRP/Statistics.py
    	renamed:    NSRP/__init__.py -> NEOPRENE/NSRP/__init__.py
    	renamed:    NSRP/inputs_simulation.py -> NEOPRENE/NSRP/inputs_simulation.py
    	renamed:    NSRP/libs_NSRP.py -> NEOPRENE/NSRP/libs_NSRP.py
    	renamed:    NSRP/outputs_simulation.py -> NEOPRENE/NSRP/outputs_simulation.py
    	renamed:    NSRP/parameters_calibration.py -> NEOPRENE/NSRP/parameters_calibration.py
    	renamed:    NSRP/utils.py -> NEOPRENE/NSRP/utils.py
    	renamed:    STNSRP/README.md -> NEOPRENE/STNSRP/README.md
    	renamed:    STNSRP/__init__.py -> NEOPRENE/STNSRP/__init__.py
    	renamed:    STNSRP/libs/MathematicalPropertiesSTNSRP.py -> NEOPRENE/STNSRP/libs/MathematicalPropertiesSTNSRP.py
    	renamed:    STNSRP/libs/PSOMdJ.py -> NEOPRENE/STNSRP/libs/PSOMdJ.py
    	renamed:    STNSRP/libs/libs_NSRP.py -> NEOPRENE/STNSRP/libs/libs_NSRP.py
    	renamed:    STNSRP/libs/libs_STNSRP.py -> NEOPRENE/STNSRP/libs/libs_STNSRP.py
    	renamed:    STNSRP/libs/libs_others.py -> NEOPRENE/STNSRP/libs/libs_others.py
    	renamed:    STNSRP/libs/main_NSRP.py -> NEOPRENE/STNSRP/libs/main_NSRP.py
    	renamed:    __init__.py -> NEOPRENE/__init__.py
    	modified:   setup.py

--- setup.py
+++ setup.py
@@ -1,30 +1,30 @@
-#!/usr/bin/env python
-
 from setuptools import setup, find_packages
 from pathlib import Path
 this_directory = Path(__file__).parent
 long_description = (this_directory / "README.md").read_text()
 
-setup(name='NEOPRENE',
-      version='0.0.6',
-      description='🌎 Scripts and information to synthetic generation of precipitation based on Point Processes.',
-      long_description=long_description,
-      long_description_content_type='text/markdown',
-      author='Javier Diez Sierra <javier.diez@unican.es>, Salvador Navas <salvador.navas@unican.es>, Manuel del Jesus <manuel.deljesus@unican.es>',
-      author_email='javier.diez@unican.es, salvador.navas@unican.es, manuel.deljesus@unican.es',
-      maintainer       = 'Manuel del Jesus',
-      maintainer_email = 'manuel.deljesus@unican.es',
-      url = 'https://github.com/IHCantabria/NEOPRENE/tree/Crear-ejecutable',
-      packages = ['NSRP','STNSRP'],
-      include_package_data = True,
-      python_requires='>=3.7, <4',
-      install_requires=[
-          'numpy',
-          'pandas',
-          'scipy',
-          'datetime',
-          'matplotlib',
-          'pyyaml',
-      ],
-      extras_require={'plotting': ['matplotlib>=2.2.0', 'jupyter','jupyterlab']}
-     )
+setup(  name='NEOPRENE',
+        packages = find_packages(),
+        license = "Apache 2.0",
+        version='0.0.10',
+        description='🌎 Scripts and information to synthetic generation of precipitation based on Point Processes.',
+        long_description=long_description,
+        long_description_content_type='text/markdown',
+        author='Javier Diez Sierra <javier.diez@unican.es>, Salvador Navas <salvador.navas@unican.es>, Manuel del Jesus <manuel.deljesus@unican.es>',
+        author_email='javier.diez@unican.es, salvador.navas@unican.es, manuel.deljesus@unican.es',
+        maintainer       = 'Manuel del Jesus',
+        maintainer_email = 'manuel.deljesus@unican.es',
+        url = 'https://github.com/IHCantabria/NEOPRENE/tree/Crear-ejecutable',
+
+        include_package_data = True,
+        python_requires='>=3.7, <4',
+        install_requires=[
+            'numpy',
+            'pandas',
+            'scipy',
+            'datetime',
+            'matplotlib',
+            'pyyaml',
+        ],
+        extras_require={'plotting': ['matplotlib>=2.2.0', 'jupyter','jupyterlab']}
+        )
